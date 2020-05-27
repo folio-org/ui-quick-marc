15010795e94355ed511cc39ce9c90a67e77dda99
Vlad Samets <uladislau_samets@epam.com>
2020-05-27 11:03:01 +0300

    f

--- src/QuickMarcEditor/QuickMarcEditorRows/IndicatorField/IndicatorField.test.js
+++ src/QuickMarcEditor/QuickMarcEditorRows/IndicatorField/IndicatorField.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, act } from '@testing-library/react';
+import { render, cleanup, act, fireEvent } from '@testing-library/react';
 
 import '@folio/stripes-acq-components/test/jest/__mock__';
 
@@ -12,14 +12,19 @@ const getComponent = () => (
 );
 
 describe('Given indicator field', () => {
+  let getByTestId;
+
   afterEach(cleanup);
 
   it('content should be focused', async () => {
-    let getByTestId;
-
     await act(async () => {
       getByTestId = await render(getComponent()).getByTestId;
     });
+
+    fireEvent(getByTestId('indicator-field'), new MouseEvent('focus', {
+      bubbles: true,
+      cancelable: true,
+    }));
 
     expect(getByTestId('indicator-field')).toBeDefined();
   });
