9084d4b456bb577551a1f7660f5a21bdf2059189
John Malconian <malc@indexdata.com>
2021-03-10 11:54:45 -0500

    update coverage report path

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const commonCofig = require('@folio/stripes-acq-components/jest.config');
 
 module.exports = {
   ...commonCofig,
-  coverageDirectory: './artifacts/coverage/',
+  coverageDirectory: './artifacts/coverage-jest/',
   collectCoverageFrom: [
     ...commonCofig.collectCoverageFrom,
     './src/*.{js,jsx}',
