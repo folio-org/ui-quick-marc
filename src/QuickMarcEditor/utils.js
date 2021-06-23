f9a815b8921fba07036adc1c1afa9fd11eed10a1
Denys Bohdan <Denys_Bohdan@epam.com>
2021-06-23 16:22:19 +0300

    UIQM-118 MARC record does NOT open after saving an invalid field

--- src/QuickMarcEditor/utils.js
+++ src/QuickMarcEditor/utils.js
@@ -141,6 +141,10 @@ export const validateRecordTag = marcRecords => {
     return 'ui-quick-marc.record.error.tag.length';
   }
 
+  if (marcRecords.some(({ tag }) => !tag.match(/\d{3}/))) {
+    return 'ui-quick-marc.record.error.tag.nonDigits';
+  }
+
   return undefined;
 };
 
