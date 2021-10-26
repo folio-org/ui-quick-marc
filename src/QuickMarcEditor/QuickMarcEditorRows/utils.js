07234d2e3fb3da7b73bbc262e345f7cee3df3566
Vladyslav Velytskyi <Vladyslav_Velytskyi@epam.com>
2021-10-26 14:30:03 +0300

    UIQM-161: Remove add button for AMRC holdings tag 004

--- src/QuickMarcEditor/QuickMarcEditorRows/utils.js
+++ src/QuickMarcEditor/QuickMarcEditorRows/utils.js
@@ -41,9 +41,14 @@ const INDICATOR_EXEPTION_ROWS = new Set([LEADER_TAG, '001', '002', '003', '004',
 
 export const hasIndicatorException = recordRow => INDICATOR_EXEPTION_ROWS.has(recordRow.tag);
 
-const ADD_EXCEPTION_ROWS = new Set([LEADER_TAG, '001', '003', '005', '008']);
+const ADD_EXCEPTION_ROWS = {
+  [MARC_TYPES.BIB]: new Set([LEADER_TAG, '001', '003', '005', '008']),
+  [MARC_TYPES.HOLDINGS]: new Set([LEADER_TAG, '001', '003', '004', '005', '008']),
+};
 
-export const hasAddException = recordRow => ADD_EXCEPTION_ROWS.has(recordRow.tag);
+export const hasAddException = (recordRow, marcType = MARC_TYPES.BIB) => {
+  return ADD_EXCEPTION_ROWS[marcType].has(recordRow.tag);
+};
 
 const DELETE_EXCEPTION_ROWS = new Set([LEADER_TAG, '001', '003', '005', '008']);
 
