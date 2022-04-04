4dbc816c70fe23f49e1c2b2f852cbafd63443a39
Denys Bohdan <Denys_Bohdan@epam.com>
2022-04-04 17:27:53 +0300

    UIQM-162 Optimistic locking: display error message to inform user about OL

--- src/QuickMarcEditor/QuickMarcEditorRows/LocationField/LocationField.js
+++ src/QuickMarcEditor/QuickMarcEditorRows/LocationField/LocationField.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {
+  useState,
+  useCallback,
+} from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
-
 import { omit } from 'lodash';
+import { FormSpy } from 'react-final-form';
 
 import { LocationLookup } from '@folio/stripes/smart-components';
 
@@ -10,20 +13,27 @@ import { QUICK_MARC_ACTIONS } from '../../constants';
 import { getLocationValue } from '../../utils';
 import { ContentField } from '../ContentField';
 
+const spySubscription = { values: true };
+
 export const LocationField = ({
+  id,
   action,
   input,
   fields,
   isLocationLookupUsed,
   setIsLocationLookupUsed,
-  permanentLocation,
-  setPermanentLocation,
   ...props
 }) => {
   const intl = useIntl();
+  const [permanentLocation, setPermanentLocation] = useState(getLocationValue(input.value));
 
   const getContentFieldValue = () => {
-    const locationField = fields.find(field => field.tag === '852');
+    const locationField = fields.find(field => field.id === id);
+
+    if (!locationField) {
+      return input;
+    }
+
     const newInput = omit(input, ['value']);
     const locationValue = getLocationValue(input.value);
 
@@ -43,6 +53,16 @@ export const LocationField = ({
     return newInput;
   };
 
+  const changeRecords = useCallback(({ values }) => {
+    if (values?.records) {
+      const locationField = values.records.find(field => field.id === id);
+
+      const matchedLocation = getLocationValue(locationField?.content);
+
+      setPermanentLocation(matchedLocation);
+    }
+  }, []);
+
   const isReplacingLocationNeeded = permanentLocation !== getLocationValue(input.value);
 
   const inputContent = isReplacingLocationNeeded
@@ -57,6 +77,7 @@ export const LocationField = ({
       />
       <LocationLookup
         label={intl.formatMessage({ id: 'ui-quick-marc.permanentLocationLookup' })}
+        marginBottom0
         onLocationSelected={location => {
           if (!isLocationLookupUsed) {
             setIsLocationLookupUsed(true);
@@ -64,7 +85,10 @@ export const LocationField = ({
 
           setPermanentLocation(`$b ${location.code}`);
         }}
-        marginBottom0
+      />
+      <FormSpy
+        subscription={spySubscription}
+        onChange={changeRecords}
       />
     </>
   );
