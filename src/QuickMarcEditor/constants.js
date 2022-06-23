7b9b010395e9657afebfd90ae1387abb00d37163
Priyanka <priyanka_terala@epam.com>
2022-06-23 15:00:27 +0530

    UIQM-246 update LDR 18 position acceptable values

--- src/QuickMarcEditor/constants.js
+++ src/QuickMarcEditor/constants.js
@@ -18,6 +18,7 @@ export const LEADER_VALUES_FOR_POSITION = {
     5: ['c', 'd', 'n'],
     6: ['u', 'v', 'x', 'y'],
     17: ['1', '2', '3', '4', '5', 'm', 'u', 'z'],
+    18: ['i', 'n'],
   },
   [MARC_TYPES.AUTHORITY]: [],
 };
