79ee190417e0282a58984e78861a0a281f3d9acd
odoniyor <doniyor_otamurodov@epam.com>
2022-09-05 13:45:34 +0500

    UIQM-272: test coverage for save & keep editing

--- src/QuickMarcEditor/QuickMarcEditWrapper.test.js
+++ src/QuickMarcEditor/QuickMarcEditWrapper.test.js
@@ -290,14 +290,43 @@ describe('Given QuickMarcEditWrapper', () => {
   afterEach(cleanup);
 
   describe('when is bib marc type', () => {
-    describe('when click on save button', () => {
-      it('should show on save message and redirect on load page', async () => {
+    describe('when click on "Save & keep editing" button', () => {
+      it('should show on save message and stay on the edit page', async () => {
         let getByText;
+        const mockOnClose = jest.fn();
 
         await act(async () => {
           getByText = renderQuickMarcEditWrapper({
             instance,
             mutator,
+            location: {
+              search: 'relatedRecordVersion=1',
+            },
+            onClose: mockOnClose,
+          }).getByText;
+        });
+
+        await act(async () => { fireEvent.click(getByText('ui-quick-marc.record.save.continue')); });
+
+        expect(mutator.quickMarcEditInstance.GET).toHaveBeenCalled();
+        expect(mutator.quickMarcEditMarcRecord.PUT).toHaveBeenCalled();
+
+        waitFor(() => expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.updated' }));
+
+        expect(mockOnClose).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('when click on save button', () => {
+      it('should show on save message and redirect on load page', async () => {
+        let getByText;
+        const mockOnClose = jest.fn();
+
+        await act(async () => {
+          getByText = renderQuickMarcEditWrapper({
+            instance,
+            mutator,
+            onClose: mockOnClose,
             location: {
               search: 'relatedRecordVersion=1',
             },
@@ -309,7 +338,9 @@ describe('Given QuickMarcEditWrapper', () => {
         expect(mutator.quickMarcEditInstance.GET).toHaveBeenCalled();
         expect(mutator.quickMarcEditMarcRecord.PUT).toHaveBeenCalled();
 
-        waitFor(() => expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.success.processing' }));
+        waitFor(() => expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.updated' }));
+
+        waitFor(() => expect(mockOnClose).toHaveBeenCalled());
       }, 1000);
 
       describe('when there is an error during POST request', () => {
@@ -445,15 +476,42 @@ describe('Given QuickMarcEditWrapper', () => {
   });
 
   describe('when is authority marc type', () => {
-    describe('when click on save button', () => {
-      it('should show on save message and redirect on load page', async () => {
+    describe('when click on "Save & keep editing" button', () => {
+      it('should show on save message and stay on the edit page', async () => {
         let getByText;
+        const mockOnClose = jest.fn();
 
         await act(async () => {
           getByText = renderQuickMarcEditWrapper({
             instance,
             mutator,
             marcType: MARC_TYPES.AUTHORITY,
+            onClose: mockOnClose,
+          }).getByText;
+        });
+
+        await act(async () => { fireEvent.click(getByText('ui-quick-marc.record.save.continue')); });
+
+        expect(mutator.quickMarcEditInstance.GET).toHaveBeenCalled();
+        expect(mutator.quickMarcEditMarcRecord.PUT).toHaveBeenCalled();
+
+        waitFor(() => expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.updated' }));
+
+        expect(mockOnClose).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('when click on save button', () => {
+      it('should show on save message and redirect on load page', async () => {
+        let getByText;
+        const mockOnClose = jest.fn();
+
+        await act(async () => {
+          getByText = renderQuickMarcEditWrapper({
+            instance,
+            mutator,
+            marcType: MARC_TYPES.AUTHORITY,
+            onClose: mockOnClose,
           }).getByText;
         });
 
@@ -463,6 +521,8 @@ describe('Given QuickMarcEditWrapper', () => {
         expect(mutator.quickMarcEditMarcRecord.PUT).toHaveBeenCalled();
 
         waitFor(() => expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.updated' }));
+
+        waitFor(() => expect(mockOnClose).toHaveBeenCalled());
       }, 1000);
 
       describe('when there is an error during POST request', () => {
