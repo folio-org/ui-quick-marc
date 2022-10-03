4f5a70430d4ef108dbac736e56ffbdd9dc51d809
Denys Bohdan <denys_bohdan@epam.com>
2022-10-03 14:37:32 +0300

    UIQM-275 Fix eslint errors

--- src/QuickMarcEditor/QuickMarcEditorRows/LinkButton/LinkButton.js
+++ src/QuickMarcEditor/QuickMarcEditorRows/LinkButton/LinkButton.js
@@ -1,7 +1,4 @@
-import {
-  useEffect,
-  useState,
-} from 'react';
+import { useState } from 'react';
 import {
   useIntl,
   FormattedMessage,
--- src/QuickMarcEditor/QuickMarcEditorRows/QuickMarcEditorRows.js
+++ src/QuickMarcEditor/QuickMarcEditorRows/QuickMarcEditorRows.js
@@ -150,7 +150,7 @@ const QuickMarcEditorRows = ({
     unlinkAuthority(fields[index]);
 
     markRecordUnlinked({ index });
-  }, [markRecordUnlinked]);
+  }, [markRecordUnlinked, unlinkAuthority, fields]);
 
   return (
     <div
