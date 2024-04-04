4635f424aee7fd459904867e51c6c78809c9ac1b
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2024-04-04 16:26:57 +0300

    UIQM-641: CallcleanBytesFields function with correct arguments to fix 008 field.

--- src/QuickMarcEditor/QuickMarcEditWrapper.test.js
+++ src/QuickMarcEditor/QuickMarcEditWrapper.test.js
@@ -11,6 +11,7 @@ import arrayMutators from 'final-form-arrays';
 import { useLocation } from 'react-router';
 
 import QuickMarcEditWrapper from './QuickMarcEditWrapper';
+import QuickMarcEditor from './QuickMarcEditor';
 import { useAuthorityLinking } from '../hooks';
 import { QUICK_MARC_ACTIONS } from './constants';
 import { MARC_TYPES } from '../common/constants';
@@ -24,6 +25,13 @@ import {
   authorityLeader,
   bibLeader,
 } from '../../test/jest/fixtures/leaders';
+import fixedFieldSpecBib from '../../test/mocks/fixedFieldSpecBib';
+
+jest.mock('./QuickMarcEditor', () => {
+  const RealQuickMarcEditor = jest.requireActual('./QuickMarcEditor').default;
+
+  return jest.fn(props => <RealQuickMarcEditor {...props} />);
+});
 
 jest.mock('./utils', () => ({
   ...jest.requireActual('./utils'),
@@ -376,6 +384,7 @@ const renderQuickMarcEditWrapper = ({
           locations={locations}
           externalRecordPath="/some-record"
           refreshPageData={jest.fn().mockResolvedValue()}
+          fixedFieldSpec={fixedFieldSpecBib}
           onCheckCentralTenantPerm={mockOnCheckCentralTenantPerm}
           {...renderProps}
           {...props}
@@ -469,6 +478,204 @@ describe('Given QuickMarcEditWrapper', () => {
 
         expect(mockShowCallout).toHaveBeenCalledWith({ messageId: 'ui-quick-marc.record.save.success.processing' });
         expect(mockOnSave).toHaveBeenCalled();
+      });
+
+      it('should derive record with correct payload', async () => {
+        renderQuickMarcEditWrapper({
+          instance,
+          mutator,
+        });
+
+        const formValues = {
+          '_actionType': 'view',
+          'leader': {
+            'Record length': '00246',
+            'Status': 'n',
+            'Type': 'a',
+            'BLvl': 'm',
+            'Ctrl': '\\',
+            '9-16 positions': 'a2200085',
+            'ELvl': 'u',
+            'Desc': 'u',
+            'MultiLvl': '\\',
+            '20-23 positions': '4500',
+          },
+          'suppressDiscovery': false,
+          'marcFormat': 'BIBLIOGRAPHIC',
+          'parsedRecordId': '2b56625f-1ca0-4ada-a32d-2667be1bd509',
+          'parsedRecordDtoId': '2b56625f-1ca0-4ada-a32d-2667be1bd509',
+          'externalId': 'e72f49c9-9bbf-4d2b-89eb-3d2ee5878530',
+          'externalHrid': 'in00000000035',
+          'updateInfo': {
+            'recordState': 'ACTUAL',
+            'updateDate': '2024-04-04T12:25:57.937Z',
+            'updatedBy': {
+              'userId': '7b51e0cd-e1cb-5715-9592-e6f9c9aa0c33',
+              'username': 'diku_admin',
+              'lastName': 'ADMINISTRATOR',
+              'firstName': 'DIKU',
+            },
+          },
+          'records': [
+            {
+              'tag': 'LDR',
+              'content': {
+                'Record length': '00246',
+                'Status': 'n',
+                'Type': 'e',
+                'BLvl': 'm',
+                'Ctrl': '\\',
+                '9-16 positions': 'a2200085',
+                'ELvl': 'u',
+                'Desc': 'u',
+                'MultiLvl': '\\',
+                '20-23 positions': '4500',
+              },
+              'id': 'LDR',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+            {
+              'tag': '001',
+              'content': 'in00000000035',
+              'isProtected': true,
+              'id': '7460b69e-ec5b-4eb3-9563-09495ee21729',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+            {
+              'tag': '005',
+              'content': '20240404122557.9',
+              'isProtected': false,
+              'id': 'a152e415-2923-47c6-9e1e-0ed3622a47b9',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+            {
+              'tag': '008',
+              'content': {
+                'Type': 'a',
+                'BLvl': 'm',
+                'Entered': '240404',
+                'DtSt': 'u',
+                'Date1': '\\\\\\\\',
+                'Date2': '\\\\\\\\',
+                'Ctry': '\\\\\\',
+                'Lang': '\\\\\\',
+                'MRec': '\\',
+                'Srce': '\\',
+                'Ills': ['p', 'o', 'm', 'l'],
+                'Audn': 'j',
+                'Form': 's',
+                'Cont': ['6', '5', '2', 'y'],
+                'GPub': 's',
+                'Conf': '0',
+                'Fest': '1',
+                'Indx': '0',
+                'LitF': 'p',
+                'Biog': '\\',
+                'SpFm': ['\\', '\\'],
+                'Relf': ['a', 'b', 'c', 'd'],
+                'Proj': '\\\\',
+                'CrTp': 'a',
+              },
+              'isProtected': false,
+              'id': '81d06703-2a0a-4cf0-87c8-a03ff0d995b5',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+            {
+              'tag': '245',
+              'content': '$a rec2',
+              'indicators': ['\\', '\\'],
+              'isProtected': false,
+              'id': '373aa74f-c1e9-4316-a47c-749d65fab1b4',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+            {
+              'tag': '999',
+              'content': '$i e72f49c9-9bbf-4d2b-89eb-3d2ee5878530 $s 2b56625f-1ca0-4ada-a32d-2667be1bd509',
+              'indicators': ['f', 'f'],
+              'isProtected': true,
+              'id': 'be587dc9-80f9-4099-a721-e58d09f4af93',
+              '_isDeleted': false,
+              '_isLinked': false,
+            },
+          ],
+        };
+        const formValuesForDerive = {
+          '_actionType': 'edit',
+          'leader': '00246nem\\a2200085uu\\4500',
+          'fields': [
+            {
+              'tag': '001',
+              'content': 'in00000000035',
+            },
+            {
+              'tag': '005',
+              'content': '20240404122557.9',
+            },
+            {
+              'tag': '008',
+              'content': {
+                'Type': 'e',
+                'BLvl': 'm',
+                'Entered': '240404',
+                'DtSt': 'u',
+                'Date1': '\\\\\\\\',
+                'Date2': '\\\\\\\\',
+                'Ctry': '\\\\\\',
+                'Lang': '\\\\\\',
+                'MRec': '\\',
+                'Srce': '\\',
+                'Audn': 'j',
+                'Form': 's',
+                'GPub': 's',
+                'Conf': '0',
+                'Fest': '1',
+                'Indx': '0',
+                'LitF': 'p',
+                'Biog': '\\',
+                'SpFm': ['\\', '\\'],
+                'Relf': ['a', 'b', 'c', 'd'],
+                'Proj': '\\\\',
+                'CrTp': 'a',
+              },
+            },
+            {
+              'tag': '245',
+              'content': '$a rec2',
+              'indicators': ['\\', '\\'],
+            },
+            {
+              'tag': '999',
+              'content': '$i e72f49c9-9bbf-4d2b-89eb-3d2ee5878530 $s 2b56625f-1ca0-4ada-a32d-2667be1bd509',
+              'indicators': ['f', 'f'],
+            },
+          ],
+          'suppressDiscovery': false,
+          'marcFormat': 'BIBLIOGRAPHIC',
+          'parsedRecordId': '2b56625f-1ca0-4ada-a32d-2667be1bd509',
+          'parsedRecordDtoId': '2b56625f-1ca0-4ada-a32d-2667be1bd509',
+          'externalId': 'e72f49c9-9bbf-4d2b-89eb-3d2ee5878530',
+          'externalHrid': 'in00000000035',
+          'updateInfo': {
+            'recordState': 'ACTUAL',
+            'updateDate': '2024-04-04T12:25:57.937Z',
+            'updatedBy': {
+              'userId': '7b51e0cd-e1cb-5715-9592-e6f9c9aa0c33',
+              'username': 'diku_admin',
+              'lastName': 'ADMINISTRATOR',
+              'firstName': 'DIKU',
+            },
+          },
+          'relatedRecordVersion': '1',
+        };
+
+        await QuickMarcEditor.mock.calls[0][0].onSubmit(formValues);
+
+        expect(mockUpdateMarcRecord).toHaveBeenCalledWith(formValuesForDerive);
       });
 
       describe('when there is an error during POST request', () => {
