c22026a15313d46a9c8b055f73e982b9049619bc
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2024-07-12 18:32:02 +0300

    UIQM-677: Add MARC Settings page.

---
+++ src/settings/QuickMarcSettings/QuickMarcSettings.js
@@ -0,0 +1,40 @@
+import { useLocation, useRouteMatch } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+
+import { TitleManager } from '@folio/stripes/core';
+import {
+  CommandList,
+  defaultKeyboardShortcuts,
+} from '@folio/stripes/components';
+import { Settings } from '@folio/stripes/smart-components';
+
+import { MarcBibTemplates } from './MarcBibTemplates';
+
+const QuickMarcSettings = () => {
+  const match = useRouteMatch();
+  const location = useLocation();
+  const { formatMessage } = useIntl();
+
+  const pages = [
+    {
+      component: MarcBibTemplates,
+      label: formatMessage({ id: 'ui-quick-marc.settings.marcBibTemplates.pane.title' }),
+      route: 'marc-bib-templates',
+    },
+  ];
+
+  return (
+    <CommandList commands={defaultKeyboardShortcuts}>
+      <TitleManager page={formatMessage({ id: 'ui-quick-marc.settings.html.page.title' })}>
+        <Settings
+          match={match}
+          location={location}
+          paneTitle={formatMessage({ id: 'ui-quick-marc.settings.heading' })}
+          pages={pages}
+        />
+      </TitleManager>
+    </CommandList>
+  );
+};
+
+export { QuickMarcSettings };
