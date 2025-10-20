baaa3e001ef242175dbfda36e7a6d74a3e096c1a
Denys Bohdan <denys_bohdan@epam.com>
2025-10-20 13:41:06 +0200

    UIQM-790 Allow to initialize quickMARC with pre-edited values.

--- src/QuickMarc.js
+++ src/QuickMarc.js
@@ -31,6 +31,7 @@ const QuickMarc = ({
   initialValues,
   onCreateAndKeepEditing = noop,
   useRoutes = true,
+  isPreEdited = false,
 }) => {
   const location = useLocation();
 
@@ -132,6 +133,7 @@ const QuickMarc = ({
               externalId={externalId}
               instanceId={instanceId}
               initialValues={initialValues}
+              isPreEdited={isPreEdited}
             />
           </QuickMarcProvider>
         )}
